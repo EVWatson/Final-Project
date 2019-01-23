0943a403a3fe780cb5c4921c1071da4fb5b52d39
EVWatson <emily.watson@myob.com>
2019-01-23 16:39:19 +1100

    seeded mongo db, created mongoose users schema, begun linking process

---
+++ api/models/users.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const schema = mongoose.schema;
+
+const usersSchema = new schema({
+      name: String,
+      date_of_birth: String,
+      parent_name: String,
+      primary_contact_name: String,
+      primary_contact_number: String,
+      address: String,
+      email: String,
+      primary_instrument: String,
+      primary_learning_location: String,
+      experience: String,
+      currently_enrolled: {
+          enrolled: Boolean,
+          day: String,
+          time: String,
+      },
+      gender: String,
+      role: String
+})
+
+module.exports = mongoose.model(usersSchema);
